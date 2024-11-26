69577fc6615655458b35aa2381f5b5ea74f8bb33
Priyanshu Kumar <priyanshu29k@gmail.com>
2024-11-26 16:55:08 +0530

    Updated index.js with new functionality

---
+++ 01/script.js
@@ -0,0 +1,5 @@
+console.log("Hi again from External  JS file");
+console.log("i love JavaScript")
+console.log("Hi there")
+
+console.log("JavaScript")
---
+++ 03/index.js
@@ -0,0 +1,49 @@
+// Data Types
+//Numbers
+//String
+
+
+let num = 10.5;
+let num2 = 13;
+console.log(num+num2-2+1500);
+
+
+// strings
+
+const firstName = 'Priyanshu';
+const lastName = 'Kumar';
+
+console.log(firstName + ' ' + lastName)
+
+
+// string '1' + '1' ---> string
+// string +number = string
+
+// string * num = number
+
+console.log('15' * 10);
+
+
+
+// Booleans
+
+let isLoggedIn = true
+console.log(isLoggedIn + 15)
+
+
+// objects
+
+const person = {
+    firstName: 'Priyanshu',
+    lastName: 'Kumar',
+    age: 19,
+    isLoggedIn: true,
+    lastLoginDate: null,
+}
+
+console.log(person);
+
+
+// 
+
+console.log(typeof '123');
