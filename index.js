638007635553dd52ae9dda3bb6b6e390bbc152ae
priyanshcpp <priyanshucpp2904@gmail.com>
2024-11-26 16:39:45 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,45 @@
+// Data Types
+//Numbers
+//String
+
+
+let num = 10.5;
+let num2 = 13;
+console.log(num+num2-2+1500);
+
+
+// strings
+
+const firstName = 'Priyanshu';
+const lastName = 'Kumar';
+
+console.log(firstName + ' ' + lastName)
+
+
+
+console.log('15' * 10);
+
+
+
+// Booleans
+
+let isLoggedIn = true
+console.log(isLoggedIn + 15)
+
+
+// objects
+
+const person = {
+    firstName: 'Priyanshu',
+    lastName: 'Kumar',
+    age: 19,
+    isLoggedIn: true,
+    lastLoginDate: null,
+}
+
+console.log(person)
+
+
+
+// operators
+
